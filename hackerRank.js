06fd1cfd22efec8eb4146b0b054287facbc5113a
Lisa Walters <lisacampiwalters@gmail.com>
2023-07-28 01:52:34 -0400

    completed second problem

--- hackerRank.js
+++ hackerRank.js
@@ -1,26 +1,45 @@
-console.log("hi");
-function matchingStrings(strings, queries) {
-    // Write your code here
-    let count = 0;
-    let result = [];
+// console.log("hi");
+// function matchingStrings(strings, queries) {
+//     // Write your code here
+//     let count = 0;
+//     let result = [];
 
-    for (let i = 0; i < queries.length; i++) {
-        count = 0;
-        for (let j = 0; j < strings.length; j++) {
-            if (queries[i] === strings[j]) {
+//     for (let i = 0; i < queries.length; i++) {
+//         count = 0;
+//         for (let j = 0; j < strings.length; j++) {
+//             if (queries[i] === strings[j]) {
+//                 count += 1;
+//             }
+//         }
+//         result.push(count);
+//     }
+//     return result;
+// }
+
+
+// let strings = ['ab', 'abc', 'ab'];
+// let queries = ['ab', 'abc', 'bc'];
+
+// let answer = matchingStrings(strings, queries);
+// console.log(answer);
+
+
+function lonelyinteger(a) {
+    // Write your code here
+    for (let i = 0; i < a.length; i++) {
+        let count = 0;
+        for (let j = 0; j < a.length; j++) {
+            if (a[i] === a[j]) {
                 count += 1;
             }
         }
-        result.push(count);
+        if (count === 1) {
+            return a[i];
+        }
     }
-    return result;
 }
 
 
-let strings = ['ab', 'abc', 'ab'];
-let queries = ['ab', 'abc', 'bc'];
-
-let answer = matchingStrings(strings, queries);
+let a = [1, 2, 3, 4, 3, 2, 1];
+let answer = lonelyinteger(a);
 console.log(answer);
-
-
